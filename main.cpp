0241e91a9a1d1450b67996715c4120fde635f732
Aldo1803 <aldoparada1803@gmail.com>
2022-08-29 17:18:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,32 @@
 #include <ctime>
 #include <map>
 
+using namespace std;
+
+map<string, int> months;
+map<int, string> monthsReversed;
+
+int monthDuration[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+struct Entry {
+    string month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+    string ip;
+    string error;
+
+    bool dateIsEqual(Entry const & entry) const {
+       if(months[month] == entry.month && day == entry.day) {
+           return true;
+       }else {
+           return false;
+
+       }
+    }
+};
+
 int main() {
   std::cout << "Hello World!\n";
 }
